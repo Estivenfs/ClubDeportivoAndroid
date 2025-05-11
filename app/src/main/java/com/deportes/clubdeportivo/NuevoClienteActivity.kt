f19ce87c8ac240f8c0f0413f3ce4d98c8b09096e
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-05-11 11:11:44 -0300

    Se agrego pop-up de "registro de cambio exitoso" y se agregó funcionalidad al botón "Registrar" de la vista Nuevo Cliente.

--- app/src/main/java/com/deportes/clubdeportivo/NuevoClienteActivity.kt
+++ app/src/main/java/com/deportes/clubdeportivo/NuevoClienteActivity.kt
@@ -1,11 +1,15 @@
 package com.deportes.clubdeportivo
 
+import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class NuevoClienteActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_cliente)
@@ -13,6 +17,7 @@ class NuevoClienteActivity : AppCompatActivity() {
         val btnMenu = findViewById<LinearLayout>(R.id.btnMenu)
         val btnConsultas = findViewById<LinearLayout>(R.id.btnConsultas)
         val btnPagos = findViewById<LinearLayout>(R.id.btnPagos)
+        val btnRegistrarCliente: Button = findViewById<Button>(R.id.btnRegistrarCliente)
 
         btnMenu.setOnClickListener {
             startActivity(Intent(this, MenuPrincipalActivity::class.java))
@@ -25,6 +30,18 @@ class NuevoClienteActivity : AppCompatActivity() {
         btnPagos.setOnClickListener {
             //Nombre de pagos activity
             //startActivity(Intent(this, PagosActivity::class.java))
+        }
+
+        btnRegistrarCliente.setOnClickListener {
+            val registroExitosoDialog =
+                RegistroExitosoFragment.newInstance() // Usar el nuevo nombre de la clase
+            registroExitosoDialog.setOnVolverClickListener {
+                // ... lógica al volver ...
+            }
+            registroExitosoDialog.show(
+                supportFragmentManager,
+                CambioExitosoFragment.TAG
+            ) // Usar el nuevo TAG
         }
     }
 
