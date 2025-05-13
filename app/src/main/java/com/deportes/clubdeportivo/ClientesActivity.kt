ff8ca8e0bfaa5907b689bb89ec7d74e20d5a9874
Lucas Tarantino <tarantinolucas@hotmail.com>
2025-05-13 07:33:47 -0300

    Se crea pantalla de clientes. Se corrije imagen de panel de administración.

--- app/src/main/java/com/deportes/clubdeportivo/ClientesActivity.kt
+++ app/src/main/java/com/deportes/clubdeportivo/ClientesActivity.kt
@@ -1,17 +1,18 @@
 package com.deportes.clubdeportivo
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.TextView
 import android.widget.ImageView
+import android.widget.LinearLayout
 
 
 class ClientesActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,15 +23,48 @@ class ClientesActivity : AppCompatActivity() {
             insets
         }
 
+        // Lógica de la barra superior
         val btnAtras: ImageView = findViewById(R.id.buttonBack)
         val textViewTitulo: TextView = findViewById(R.id.textViewTitle)
 
-
-        // Logica
         textViewTitulo.text = "Clientes"
 
         btnAtras.setOnClickListener {
             finish()
         }
+
+        // Lógica de la barra inferior
+
+        val btnMenu = findViewById<LinearLayout>(R.id.btnMenu)
+        val btnConsultas = findViewById<LinearLayout>(R.id.btnConsultas)
+        val btnPagos = findViewById<LinearLayout>(R.id.btnPagos)
+        val btnRegistrarCliente = findViewById<LinearLayout>(R.id.btnNuevoCliente)
+
+        btnMenu.setOnClickListener {
+            startActivity(Intent(this, MenuPrincipalActivity::class.java))
+        }
+
+        btnConsultas.setOnClickListener {
+            startActivity(Intent(this, ConsultasActivity::class.java))
+        }
+
+        btnPagos.setOnClickListener {
+            //Nombre de pagos activity
+            //startActivity(Intent(this, PagosActivity::class.java))
+        }
+
+        btnRegistrarCliente.setOnClickListener {
+            val registroExitosoDialog =
+                RegistroExitosoFragment.newInstance() // Usar el nuevo nombre de la clase
+            registroExitosoDialog.setOnVolverClickListener {
+                // ... lógica al volver ...
+            }
+            registroExitosoDialog.show(
+                supportFragmentManager,
+                CambioExitosoFragment.TAG
+            ) // Usar el nuevo TAG
+        }
+
+
     }
 }
