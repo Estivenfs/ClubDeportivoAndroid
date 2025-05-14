6268c2c4ae5c4b4d69669f6c1064a0363346a774
Sebanadin <126617199+Sebanadin@users.noreply.github.com>
2025-05-14 19:39:36 -0300

    cree la clase CarnetActivity y CarnetDetalleActivity y lo vincule a la clase ConsultaAtivity

---
+++ app/src/main/java/com/deportes/clubdeportivo/CarnetActivity.kt
@@ -0,0 +1,40 @@
+package com.deportes.clubdeportivo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CarnetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_carnet)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Importación y lógica de la barra superior
+        val btnAtras: ImageView = findViewById(R.id.buttonBack)
+        val textViewTitulo: TextView = findViewById(R.id.textViewTitle)
+        textViewTitulo.text = "Carnet"
+
+        btnAtras.setOnClickListener {
+            finish()
+        }
+
+        val btnBuscar = findViewById<Button>(R.id.btnBuscar)
+        btnBuscar.setOnClickListener {
+            // Lógica para manejar el clic en el botón "Buscar"
+            // Cambiar a activity correspondiente
+            startActivity(Intent(this, CarnetDetalleActivity::class.java))
+        }
+
+    }
+}
