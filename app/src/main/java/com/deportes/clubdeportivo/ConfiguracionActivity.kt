c202aaaf1cc62cab70d5f82d3545d6c72bd6f5ea
jose colombo <fedesaico@hotmail.com>
2025-05-15 23:55:25 -0300

    Agregado botón guardar cambios y pop up En la pantalla Precios > Actividades los precios de las actividades en vez de los días de la semana Arreglada pantallas Precios > Cuota mensual y Precios > Descuentos Ajustado botones consultas

--- app/src/main/java/com/deportes/clubdeportivo/ConfiguracionActivity.kt
+++ app/src/main/java/com/deportes/clubdeportivo/ConfiguracionActivity.kt
@@ -1,12 +1,23 @@
 package com.deportes.clubdeportivo
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ConfiguracionActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
 
+        val botonGuardar = findViewById<Button>(R.id.btnGuardarCambios)
+
+        botonGuardar.setOnClickListener {
+
+            val dialog = GuardadoExitosoFragment.newInstance()
+            dialog.setOnVolverClickListener {
+            }
+            dialog.show(supportFragmentManager, GuardadoExitosoFragment.TAG)
+        }
     }
 }
